edc1f0c9125e1ca89cf93d340f66a964d653c205
Gabriel NIERAT <fareanor.kelfasquin@gmail.com>
2022-01-04 11:16:51 +0100

    Vector.c completed

--- SimplePhysicsEngine/SimplePhysicsEngine/Utils/Vector.c
+++ SimplePhysicsEngine/SimplePhysicsEngine/Utils/Vector.c
@@ -1,8 +1,9 @@
-﻿#include "Vector.h"
+﻿#include "vector.h"
+#include <math.h>
 
 Vec2 Vec2_Set(float x, float y)
 {
-    Vec2 v = { .x = x, .y = y };
+    Vec2 v = { v.x = x, v.y = y };
     return v;
 }
 
@@ -15,30 +16,41 @@ Vec2 Vec2_Perp(Vec2 v)
 
 Vec2 Vec2_Add(Vec2 v1, Vec2 v2)
 {
+    v1.x += v2.x;
+    v1.y += v2.y;
     return v1;
 }
 
 Vec2 Vec2_Sub(Vec2 v1, Vec2 v2)
 {
+    v1.x -= v2.x;
+    v1.y -= v2.y;
     return v1;
 }
 
 Vec2 Vec2_Scale(Vec2 v, float s)
 {
+    v.x *= s;
+    v.y *= s;
     return v;
 }
 
 float Vec2_Length(Vec2 v)
 {
-    return 0.0f;
+    float len = sqrt(pow(v.x,2) + pow(v.y,2));
+    return len;
 }
 
 Vec2 Vec2_Normalize(Vec2 v)
 {
+    float len = Vec2_Length(v);
+    v.x /= len;
+    v.y /= len;
     return v;
 }
 
 float Vec2_Distance(Vec2 v1, Vec2 v2)
 {
-    return 0.0f;
+    float distance = sqrt(pow((v2.x - v1.x),2) + pow((v2.y - v1.y),2));
+    return distance;
 }
